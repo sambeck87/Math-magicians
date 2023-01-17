2aa6668ad6f2a37287a0b2a3c9fbefc2acf45ea6
sambeck87 <sandro_mipb@hotmail.com>
2023-01-17 13:23:46 -0600

    Create full website for the math magician app Create styles folder Add calculator.css file Add home.css file Add quote.css file Create pages folder Add home webpage Add quote webpage Add Header component

---
+++ src/Pages/Quote.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/quote.css';
+/* eslint-disable max-len */
+
+const Quote = () => (
+  <div id="quote_container">
+    <p id="quote">
+      &ldquo;Mathematics is not just about numbers, equations, computations, It is also about the power of reasoning, creativity, critical thinking and problem-solving ability.&ldquo; -William Paul Thurston
+    </p>
+  </div>
+);
+
+export default Quote;
